47022fd4c7020643497fb5593918f381bb4d0ade
Naomi <naomi172839@gmail.com>
2019-08-13 04:18:11 -0400

    Added copyright statement as a header

--- src/edu/bonnin/projectone/Main.java
+++ src/edu/bonnin/projectone/Main.java
@@ -1,12 +1,15 @@
-//**********************************************************************************************************************
-//Author: Naomi Bonnin
-//Course: CMIS 242 6383
-//Project Name: Project 1 (Employee Salary)
-//Modification Date: 8/12/2019
-//Purpose: The purpose of this program is to calculate and display the annual salaries for three different classes of
-//         employees.  It also calculates the average by the year.
-//Filename: Main.java
-//**********************************************************************************************************************
+////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2019.
+// Author: Naomi Bonnin
+// Course: CMIS 242 6383
+// Project Name: Project 1 (Employee Salary)
+// Filename: Main.java
+// Updated: 8/13/19, 4:18 AM
+// Description: This program loads information about employees from a " " delimited text file.
+// It then sorts and displays that information in the console.
+// Finally, it calculates and displays the average salaries for each year.
+////////////////////////////////////////////////////////////////////////////////
+
 
 package edu.bonnin.projectone;
 
--- test/edu/bonnin/projectone/SalesmanTest.java
+++ test/edu/bonnin/projectone/SalesmanTest.java
@@ -1,3 +1,15 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2019.
+// Author: Naomi Bonnin
+// Course: CMIS 242 6383
+// Project Name: Project 1 (Employee Salary)
+// Filename: SalesmanTest.java
+// Updated: 8/13/19, 4:18 AM
+// Description: This program loads information about employees from a " " delimited text file.
+// It then sorts and displays that information in the console.
+// Finally, it calculates and displays the average salaries for each year.
+////////////////////////////////////////////////////////////////////////////////
+
 package edu.bonnin.projectone;
 
 import org.junit.jupiter.api.Test;
